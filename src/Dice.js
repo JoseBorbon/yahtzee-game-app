9de6861b768089cd1b552b52f1b537f39b0e47c0
JoseBorbon <joserborbon@gmail.com>
2021-10-27 15:39:56 -0400

    refactor for readability in Dice renders return

--- src/Dice.js
+++ src/Dice.js
@@ -4,19 +4,16 @@ import './Dice.css';
 
 class Dice extends Component {
   render() {
-    return (
-      <div className="Dice">
-        {this.props.dice.map((d, idx) => (
-          <Die
-            handleClick={this.props.handleClick}
-            val={d}
-            locked={this.props.locked[idx]}
-            idx={idx}
-            key={idx}
-          />
-        ))}
-      </div>
-    );
+    const dices = this.props.dice.map((d, idx) => (
+      <Die
+        handleClick={this.props.handleClick}
+        val={d}
+        locked={this.props.locked[idx]}
+        idx={idx}
+        key={idx}
+      />
+    ));
+    return <div className="Dice">{dices}</div>;
   }
 }
 
