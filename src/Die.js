e3b47b5e364ef77bb0c57e6132b92d6022947487
JoseBorbon <joserborbon@gmail.com>
2021-10-27 15:41:25 -0400

    add toggleLock to pass in index argument

--- src/Die.js
+++ src/Die.js
@@ -2,12 +2,20 @@ import React, { Component } from 'react';
 import './Die.css';
 
 class Die extends Component {
+  constructor(props) {
+    super(props);
+    this.toggleLock = this.toggleLock.bind(this);
+  }
+  toggleLock() {
+    this.props.handleClick(this.props.idx);
+  }
+
   render() {
     return (
       <button
         className={'Die'}
         style={{ backgroundColor: this.props.locked ? 'grey' : 'black' }}
-        onClick={this.props.handleClick}
+        onClick={this.toggleLock}
       >
         {this.props.val}
       </button>
