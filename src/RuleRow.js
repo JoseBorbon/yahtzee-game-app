9a21a62f23efaa1a494278a9d06d080de94e3c80
JoseBorbon <joserborbon@gmail.com>
2021-10-27 17:47:28 -0400

    bind this keyword and add handleRulelock method

--- src/RuleRow.js
+++ src/RuleRow.js
@@ -2,10 +2,19 @@ import React, { Component } from 'react';
 import './RuleRow.css';
 
 class RuleRow extends Component {
+  constructor(props) {
+    super(props);
+    this.handleRuleLock = this.handleRuleLock.bind(this);
+  }
+  handleRuleLock() {
+    this.props.doScore();
+    this.props.handleRuleLock(this.props.idx);
+  }
   render() {
     return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+      <tr className="RuleRow RuleRow-active" onClick={this.handleRuleLock}>
         <td className="RuleRow-name">{this.props.name}</td>
+        {/* Display description for rule, then ONCE LOCKED display score*/}
         <td className="RuleRow-score">{this.props.score}</td>
       </tr>
     );
