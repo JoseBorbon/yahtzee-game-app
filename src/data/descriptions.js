08b5683e26de44cf02470e1400791aefed93de13
JoseBorbon <joserborbon@gmail.com>
2021-10-27 20:48:07 -0400

    add descriptions data

---
+++ src/data/descriptions.js
@@ -0,0 +1,17 @@
+const descriptions = [
+  'Ones: Get as many ones as possible.',
+  'Twos: Get as many twos as possible.',
+  'Threes: Get as many threes as possible.',
+  'Fours: Get as many fours as possible.',
+  'Fives: Get as many fives as possible.',
+  'Sixes: Get as many sixes as possible.',
+  'Three of a kind: Get three dice with the same number.',
+  'Four of a kind: Get four dice with the same number.',
+  'Full house: Get three of a kind and a pair, Scores 25 points.',
+  'Small straight: Get four sequential dice, Scores 30 points.',
+  'Large straight: Get five sequential dice, Scores 40 points.',
+  'YAHTZEE: Five of a kind. Scores 50 points.',
+  'Chance: The score is simply the sum of the dice.',
+];
+
+export { descriptions };
